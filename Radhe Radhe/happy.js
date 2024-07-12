83cf6a19ef7f7489cfaf70fb9c198a640ac24f60
Sachin kumar <sachinkumardev08@gmail.com>
2024-07-12 12:04:45 +0530

    Update happy.js

--- Radhe Radhe/happy.js
+++ Radhe Radhe/happy.js
@@ -2,16 +2,3 @@
  console.log("Tu bas karm karta ja  fal ki chinta tu mat kr")
 
 
-// …or create a new repository on the command line
-//  git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Sachin-kumar987/Radhe-Radhe.git
-// git push -u origin main
-
-
-// …or push an existing repository from the command line
-// git remote add origin https://github.com/Sachin-kumar987/Radhe-Radhe.git
-// git branch -M main
-// git push -u origin main
